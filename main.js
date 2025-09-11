415281b1f68910079c54415ff93fb7ee8f7109e6
A <augustinovandre@gmail.com>
2025-09-11 07:02:46 +0200

    Layout generated

---
+++ main.js
@@ -0,0 +1,9 @@
+const colors = [];
+const ROWS = 10;
+const SLOTS = 4;
+const board = document.getElementById("board");
+const palette = document.getElementById("palette")
+
+function createRow(i){
+
+}
